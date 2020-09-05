eeb4d48a9e5d7d4ac9c50dd3ad3a221093bf0fae
xaiphyr <arcadia.initiative@gmail.com>
2020-09-05 16:10:17 +0800

    initial

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php ini_set('display_errors', '1') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>&lt;R&gt;</title>
+</head>
+
+<body class="darkmode">
+    <div class="container">
+        <div class="float-right">
+            <div class="nav">
+                <div class="nav-btn">
+                    <a href="javascript:void(0)">
+                        <i class="fa fa-bars" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="nav-hide">
+                    <a href="" title="Home">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="nav-hide" title="Information">
+                    <a href="">
+                        <i class="fa fa-info-circle" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="nav-hide" title="User">
+                    <a href="">
+                        <i class="fa fa-user-circle" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="logo">
+            <span>&lt;</span>
+            R
+            <span>&gt;</span>
+        </div>
+    </div>
+
+    <div class="container-body"></div>
+    <div class="content">
+        <div class="text-center">
+            <div class="l1">R</div>
+            <div class="l2">L</div>
+            <span>&nbsp;</span>
+            <div class="l3">L</div>
+            <div class="l4">O</div>
+            <div class="l5">Z</div>
+        </div>
+        <div class="text-center">
+            <div class="content-b">
+                <p>
+                    my first try creating my own css without the help of any framework, it's a bit hard to do especially if you are not really good with designing yourself, you have to calculate every animation for the desired output, perhaps working or practicing more might help me achieve one day a really good animation with pure css, but for the mean time its crappy but its hard work.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
